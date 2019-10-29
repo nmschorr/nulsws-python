3d8021ce0f1bba1f4cbf9e8389772f41a6f5c4bc
Nancy Schorr <nms@schorrmedia.com>
2019-10-28 19:24:41 -0700

    my msg

--- qtfirst.py
+++ qtfirst.py
@@ -9,45 +9,63 @@ import PySide2.QtNetwork
 import PySide2.QtWebSockets
 import PySide2.QtCore
 import PySide2.examples
+import sys
+
 
 mode1 = PySide2.QtCore.QUrl.ParsingMode.TolerantMode
 host1 = "127.0.0.1"
 port1 = 18003
 scheme1 = "http"
 
+sockUrl = PySide2.QtCore.QUrl()
+sockUrl.setHost(host1, mode1)
+sockUrl.setScheme(scheme1)
+sockUrl.setPort(port1)
+
+myNetworkReq = PySide2.QtNetwork.QNetworkRequest()
+my_header_type = PySide2.QtNetwork.QNetworkRequest.ContentTypeHeader
+myNetworkReq.setHeader(my_header_type, 'POST')
+
+
 print("starting...")
-pqsock = PySide2.QtWebSockets.QWebSocket()
-
-pq = PySide2.QtCore.QUrl()
-pq.setHost(host1, mode1)
-pq.setScheme(scheme1)
-pq.setPort(port1)
 
 
-x = PySide2.QtWebSockets.QWebSocket.open(pqsock, pq)
+myWebSocket = PySide2.QtWebSockets.QWebSocket()
+myWebSocket.setProperty("id", "socket")
+
+
+myWebSocket.open(myNetworkReq)
+
+
+c = PySide2.QtWebSockets.QWebSocket.connect
+
+myWebSocket.sendTextMessage("hello")
+
+
+
+
+
+x = PySide2.QtWebSockets.QWebSocket.open(myWebSocket, sockUrl)
 print("Sending 'Hello, World'...")
-myint = pqsock.sendTextMessage("Hello, World")
+myint = myWebSocket.sendTextMessage("Hello, World")
 print("send result: ", myint)
 
-nr = PySide2.QtNetwork.QNetworkRequest()
-my_header_type = PySide2.QtNetwork.QNetworkRequest.ContentTypeHeader
 
-nr.setHeader(my_header_type, 'POST')
 
 
 data1 = '{"jsonrpc":"2.0","method":"getChainInfo","params”:[], “id":1234}'
 #myint2 = pqsock.sendTextMessage(my_header_type)
 
-myint3 = pqsock.sendTextMessage(data1)
+myint3 = myWebSocket.sendTextMessage(data1)
 print("send result: ", myint3)
 
 
-x = pqsock.request()
+x = myWebSocket.request()
 
 new_val = PySide2.QtWebSockets.QWebSocket.textMessageReceived
 
 print("Sent")
-pqsock.close()
+myWebSocket.close()
 
 
 
