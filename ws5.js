50fdb3ba7ba11b4e3b497550917db6150d7a57f4
Nancy Schorr <nms@schorrmedia.com>
2019-10-31 04:11:55 -0700

    fixed

--- ws5.js
+++ ws5.js
@@ -1,19 +1,86 @@
+// https://medium.com/@martin.sikora/node-js-websocket-simple-chat-tutorial-2def3a841b61
+//"use strict";
 
-const http = require('http');
-const static = require('node-static');
-const file = new static.Server('./');
-const server = http.createServer((req, res) => {
-  req.addListener('end', () => file.serve(req, res)).resume();
-});
-const port = 9001;
-server.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+//process.title = 'node-chat';
+var webSocketsServerPort = 9002;
+var webSocketServer = require('websocket').server;
+var http = require('http');
 
-server.on('upgrade', (req, socket) => {
-// Make sure that we only handle WebSocket upgrade requests
-if (req.headers['upgrade'] !== 'websocket') {
-  socket.end('HTTP/1.1 400 Bad Request');
-  return;
+var history = [ ];
+var clients = [ ];
+
+function htmlEntities(str) {
+  return String(str)
+      .replace(/&/g, '&amp;').replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
+
+var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
+
+colors.sort(function(a,b) { return Math.random() > 0.5; } );
+
+var server = http.createServer(function(request, response) {
+});
+
+server.listen(webSocketsServerPort, function() {
+  console.log((new Date()) + " Server is listening on port "
+      + webSocketsServerPort);
+});
+
+
+var wsServer = new webSocketServer({
+  httpServer: server
+});
+
+wsServer.on('request', function(request) {
+  console.log((new Date()) + ' Connection from origin '
+      + request.origin + '.');
+
+  var connection = request.accept(null, request.origin);
+  var index = clients.push(connection) - 1;
+  var userName = false;
+  var userColor = false;  console.log((new Date()) + ' Connection accepted.');
+  if (history.length > 0) {
+    connection.sendUTF(
+        JSON.stringify({ type: 'history', data: history} ));
+  }
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+
+    if (userName === false) {
+        userName = htmlEntities(message.utf8Data);
+        userColor = colors.shift();
+        connection.sendUTF(
+            JSON.stringify({ type:'color', data: userColor }));
+        console.log((new Date()) + ' User is known as: ' + userName
+                    + ' with ' + userColor + ' color.');      } else {
+        console.log((new Date()) + ' Received Message from '
+                    + userName + ': ' + message.utf8Data);
+
+        var obj = {
+          time: (new Date()).getTime(),
+          text: htmlEntities(message.utf8Data),
+          author: userName,
+          color: userColor
+        };
+        history.push(obj);
+        history = history.slice(-100);
+        var json = JSON.stringify({ type:'message', data: obj });
+        for (var i=0; i < clients.length; i++) {
+          clients[i].sendUTF(json);
+        }
+      }
+    }
+  });  // user disconnected
+
+  connection.on('close', function(connection) {
+    if (userName !== false && userColor !== false) {
+      console.log((new Date()) + " Peer "
+          + connection.remoteAddress + " disconnected.");
+      clients.splice(index, 1);
+      colors.push(userColor);
+    }
+  });
 });
 
 
@@ -22,6 +89,38 @@ if (req.headers['upgrade'] !== 'websocket') {
 
 
 
+
+
+
+
+//
+//
+//
+//
+//
+//const http = require('http');
+//const static = require('node-static');
+//const file = new static.Server('./');
+//const server = http.createServer((req, res) => {
+//  req.addListener('end', () => file.serve(req, res)).resume();
+//});
+//const port = 9001;
+//server.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+//
+//server.on('upgrade', (req, socket) => {
+//// Make sure that we only handle WebSocket upgrade requests
+//if (req.headers['upgrade'] !== 'websocket') {
+//  socket.end('HTTP/1.1 400 Bad Request');
+//  return;
+//}
+//});
+//
+//
+//
+//
+//
+//
+//
 
 //
 //const http = require('http');
