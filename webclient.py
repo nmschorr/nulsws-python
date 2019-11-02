4f4d716b3e4923508af41d4b5125852ee5f2f3e1
nmschorr <12592910+nmschorr@users.noreply.github.com>
2019-11-01 23:29:02 -0700

    minor changes

--- webclient.py
+++ webclient.py
@@ -7,68 +7,54 @@ import requests
 
 class webclient:
     def __init__(self):
-        uri4: str = 'ws://localhost:18003'
-        uri92: str = 'ws://localhost:9002'
-        uri9h: str = 'http://127.0.0.1:80'
-        uri5 = "ws://127.0.0.1:80"
-        self.uri = uri5
-        self.ws_uri: websockets.uri = websockets.parse_uri(self.uri)
+        self.json_q = None
 
-    def rem_brackets(self, myvar):
-        return myvar[1:][:-1]
+        self.uri: str = "ws://127.0.0.1:80"
+        self.post_str = None
 
-    def init_strings(self):
-        import re
-        one_str: str = 'POST HTTP/1.1'
-        one_json = json.dumps(one_str, separators=('\n', ': '))
-        two_d: dict = {'Content-Type' : 'application/json;charset=UTF-8'}
-        three_d: dict = { "Host" : "127.0.0.1" , "Upgrade": "websocket", "Connection" : "Upgrade"}
-        four_d: dict = {'Accept' : '*/*', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive'}
-        two_three_four_dict: dict = {**two_d, **three_d, **four_d}
-
-        two_three_four_json = json.dumps(two_three_four_dict, separators=('\n', ': ')  )[1:][:-1]
+    def init_strings(self) -> str:
+        # one_str: str = 'POST HTTP/1.1'
+        # one_json = json.dumps(one_str, separators=('\n', ': '))
+        # two_d: dict = {'Content-Type': 'application/json;charset=UTF-8'}
+        # three_d: dict = {"Host": "127.0.0.1", "Upgrade": "websocket", "Connection": "Upgrade"}
+        # four_d: dict = {'Accept': '*/*', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive'}
+        # two_three_four_dict: dict = {**two_d, **three_d, **four_d}
+        # two_three_four_json = json.dumps(two_three_four_dict, separators=(',', ':'))
                                                 # above [1:][:-1] removes the brackets
-        all_json = one_json + "\n" + two_three_four_json
-        clean_json_header: str =  re.sub(r'"', "", all_json)
+        # all_json = one_json + "\n" + two_three_four_json
+        # clean_json_header: str =  re.sub(r'"', "", all_json)
 
-        #print(clean_json_header)
-        print()
+        query_dict: dict = {'jsonrpc': '2.0', 'method': 'getChainInfo', "params": [], "id": 1234}
+        query_j: str = json.dumps(query_dict, indent=3, separators=(',', ': '))
+        # self.post_str = clean_json_header + "\n\n" + query_json
+        return query_j
 
 
-        query_dict: dict = {'jsonrpc': '2.0',  'method': 'getChainInfo' , "params" : [], "id" : 1234 }
-        query_json: str = json.dumps(query_dict, indent=3, separators=(',', ': ') )
-        self.post_str = clean_json_header + "\n\n" + query_json
-        #print("post_str: ", self.post_str)
-
-        # send_this = '\{ \'POST  HTTP/1.1', 'Host: 127.0.0.1:18003', 'Content-Type: application/json;charset=UTF-8', 'Accept: */*', 'Cache-Control: no-cache' ,'Host: 127.0.0.1:18003', 'Accept-Encoding: gzip deflate' ,'Content-Length: 80' ,'Connection: keep-alive', 'cache-control: no-cache', 'jsonrpc: 2.0' , 'method : getChainInfo',  'params:[]', 'id: 1234' \}'
-
-    async def json_runner_post(self, uri, json_query):
-        async with websockets.connect(uri) as websocket_js:
-
-            print(f'Sending post {json_query}. Waiting for post answer')
-            await websocket_js.send(json_query)
-            json_resp = await websocket_js.recv()
+    async def run_me(self):
+        async with websockets.connect(self.uri) as wshs:
+            print(f'Sending post. Waiting for post answer')
+            # the_str: str = "hi"
+            await wshs.send(self.json_q)
+            json_resp = await wshs.recv()
             print(f'Received post response: {json_resp}')
 
-
-
-    async def run_handshake(self):
-        async with websockets.connect(self.uri) as websocket_hs:
-            await websocket_hs.handshake(self.ws_uri)
-
-
     def main(self):
-        self.init_strings()
-
-        asyncio.get_event_loop().run_until_complete(self.run_handshake())
-
+        self.json_q = self.init_strings()
+        print("sending data...")
+        asyncio.get_event_loop().run_until_complete(self.run_me())
         print('Program done')
 
-        # asyncio.get_event_loop().run_until_complete(self.json_runner_post( self.uri, self.post_str ))
-        # asyncio.get_event_loop().run_forever(self.json_runner_post( self.uri, self.post_str ))
 
 w = webclient()
 w.main()
+
+
+
+
+
+# send_this = '\{ \'POST  HTTP/1.1', 'Host: 127.0.0.1:18003', 'Content-Type: application/json;charset=UTF-8', 'Accept: */*', 'Cache-Control: no-cache' ,'Host: 127.0.0.1:18003', 'Accept-Encoding: gzip deflate' ,'Content-Length: 80' ,'Connection: keep-alive', 'cache-control: no-cache', 'jsonrpc: 2.0' , 'method : getChainInfo',  'params:[]', 'id: 1234' \}'
+
+# self.ws_uri: websockets.uri = websockets.parse_uri(self.uri)
 
 # websocket_hs.handshake()
 # hs_str = self.handshake_str
@@ -78,8 +64,8 @@ w.main()
 # print(f'JUST got response: {my_answer_hs}')
 
 
-
-
+# asyncio.get_event_loop().run_until_complete(self.json_runner_post( self.uri, self.post_str ))
+# asyncio.get_event_loop().run_forever(self.json_runner_post( self.uri, self.post_str ))
 
 
 
