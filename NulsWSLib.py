0ad94ea47e15587ebbca49a941003e5e289a64b5
Nancy Schorr <nms@schorrmedia.com>
2019-12-03 03:48:05 -0800

    Message One just about done

---
+++ NulsWSLib.py
@@ -0,0 +1,90 @@
+#!usr/bin/python3.7
+
+# Messages have a common structure composed of six fields:
+# •  ProtocolVersion: version the service to understand,2 numbers, major/minor
+# •  MessageID: identifies a request.
+# •  Timestamp:  Number  of  seconds  since  epoch January 1,1970
+# •  TimeZone: The time zone where the request was originated
+# •  MessageType: The message type, these are specified on section 3
+# •  MessageData: A Json object with the message payload
+
+# the first object that should be sent - only if the negotiation is ok service may process
+# further requests -otherwise a NegotiateConnectionResponse object should be received with
+# Status set to 0 (Failure) and disconnect immediately.
+
+# "MessageType": "NegotiateConnection",
+# "MessageData": {
+#     "CompressionAlgorithm": "zlib",
+#     "CompressionRate": "3"
+
+## -- Enter your custom settings data via the library file nulsuserone.py
+# Note: Don't use typing.Dict - it can cause json problems when converted
+# This file right now is the client only.
+# author:  Nancy M Schorr, for Nuls
+# December 2, 2019
+
+from time import time, timezone
+import json
+
+
+def prep_data1(c_alg, cr_intstr):
+    # return {"MessageID": m_id, "Timestamp": t_stamp, "TimeZone": tzone, "MessageType": mtype}
+    mid_name = "MessageID"
+    ts_name = "Timestamp"
+    tz_name = "TimeZone"
+    msg_type_label = "MessageType"
+    msg_type = "NegotiateConnection"
+    msg_data_label = "MessageData"
+    ca_label = "CompressionAlgorithm"
+    cr_label = "CompressionRate"
+    the_time = time()
+    tz = timezone
+
+    t_stamp = str(int(the_time * 1000))       # change float to int
+    m_id = str(int(the_time * 100000))     # change float to int
+    tzone = str(int(tz / 3600))   # change float to int to str
+    jlist = { mid_name: m_id,
+              ts_name: t_stamp,
+              tz_name: tzone,
+              msg_type_label: msg_type,
+              msg_data_label: {ca_label : c_alg, cr_label : cr_intstr}
+             }
+    json_str = json.dumps(jlist, separators=(',', ':'))
+    return json_str
+
+def myprint( x, y=None):
+    debug = True
+    if y is not None:
+        x = x + ' ' + y
+    if debug:
+        print(x)
+
+def prep_async( t_vars):
+    ztype = t_vars[0]
+    zcomp = t_vars[1]
+    good_msg = "all is good, was 1, ok to continue"
+
+    if ztype != 'zlib':  # neg_conn
+        ztype = 'zlib'
+    return ztype, zcomp, good_msg
+
+def check_answer(answer) -> bool:
+    msg_data_lab = "MessageData"
+    msg_negotiate_lab = 'NegotiationStatus'
+    mtyp_lab = "MessageType"
+    mt_good_answr = "NegotiateConnectionResponse"
+    j = json.loads(answer)
+    print(j)
+
+    msg_d_answer = j.get(msg_data_lab)
+    mt = j.get(mtyp_lab)
+
+    if mt == mt_good_answr:
+        final_int = msg_d_answer.get(msg_negotiate_lab)
+        if final_int == 1:
+            print("all is good")
+            return True
+        else:
+            return False
+
+
--- cl.py
+++ cl.py
@@ -1,126 +1,64 @@
 #!usr/bin/python3.7
 
-# All messages will have a common base structure composed of six fields:
-# •  ProtocolVersion: Represents the protocol version that the caller needs the
-# service to understand, it is composed by two numbers, major and minor and
-# follows semantic rules, which means that if the major number is different the
-# connection is refused, if the minor number varies then a successful
-# connection can be established.
-# •  MessageID: This is a string that identifies a request. Its length should not
-# surpass 256 characters.
-# •  Timestamp:  Number  of  seconds  since  epoch  (January  1,  1970
-# at 00:00:00 GMT)
-# •  TimeZone: The time zone where the request was originated and it is
-# represented as a number between -12 and 12
-# •  MessageType: The message type, these are specified on section 3]
-# •  MessageData: A Json object that holds the payload of the message.
-
-# Note: be careful with typing.Dict - it can cause json problems when converted
+# Messages have a common structure composed of six fields:
+# •  ProtocolVersion: version the service to understand,2 numbers, major/minor
+# •  MessageID: identifies a request.
+# •  Timestamp:  Number  of  seconds  since  epoch January 1,1970
+# •  TimeZone: The time zone where the request was originated
+# •  MessageType: The message type, these are specified on section 3
+# •  MessageData: A Json object with the message payload
 
 # the first object that should be sent - only if the negotiation is ok service may process
 # further requests -otherwise a NegotiateConnectionResponse object should be received with
 # Status set to 0 (Failure) and disconnect immediately.
+
 # "MessageType": "NegotiateConnection",
 # "MessageData": {
 #     "CompressionAlgorithm": "zlib",
 #     "CompressionRate": "3"
 
 ## -- Enter your custom settings data via the library file nulsuserone.py
+# Note: Don't use typing.Dict - it can cause json problems when converted
+# This file right now is the client only.
+# author:  Nancy M Schorr, for Nuls
+# December 2, 2019
 
 
-from tornado.websocket import websocket_connect, WebSocketClosedError
-from json import load, dumps
 from asyncio import run
-from time import time, timezone
-import nulsuserone
-import json
+from tornado.websocket import websocket_connect, WebSocketClosedError
+import NulsWSLib as nw
+import nulsuserone as nu
 
 class NulsWebsocket(object):
     def __init__(self):
-        self.ws_url = nulsuserone.my_url
-        self.myprint("the url:  ", self.ws_url)
-
-    def prep_data1(self, c_alg, cr_intstr):
-        the_time = time()
-        tz = timezone
-        mid_name = "MessageID"
-        ts_name = "Timestamp"
-        tz_name = "TimeZone"
-        msg_type_label = "MessageType"
-        msg_type = "NegotiateConnection"
-        msg_data_label = "MessageData"
-        ca_label = "CompressionAlgorithm"
-        cr_label = "CompressionRate"
-
-
-        t_stamp = str(int(the_time * 1000))       # change float to int
-        m_id = str(int(the_time * 100000))     # change float to int
-        tzone = str(int(tz / 3600))   # change float to int to str
-        jlist = { mid_name: m_id,
-                  ts_name: t_stamp,
-                  tz_name: tzone,
-                  msg_type_label: msg_type,
-                  msg_data_label: {ca_label : c_alg, cr_label : cr_intstr}
-                 }
-        return jlist
-                #return {"MessageID": m_id, "Timestamp": t_stamp, "TimeZone": tzone, "MessageType": mtype}
-
-    def myprint(self, x, y=None):
-        debug = True
-        if y is not None:
-            x = x + ' ' + y
-        if debug == True:
-            print(x)
-
-    # def one_lib(self, comp_name = 'zlib', comp_int = 3):
-    #     self.send_negotiate_conn()
+        nw.myprint("the url:  ", self.nu.my_url)
 
     async def connect_serve1(self, t_vars):
-        ztype = t_vars[0]
-        zcomp = str(t_vars[1])
-
         try:
-            if ztype != 'zlib':  # neg_conn
-                ztype = 'zlib'
-            connection = await websocket_connect(nulsuserone.my_url )
+            ztype, zcomp, good_msg = nw.prep_async()
+            connection = await websocket_connect(nu.my_url )
                 ### only continue if connection is ok
-            js_list = self.prep_data1(ztype, zcomp)
-            json_str = json.dumps(js_list, separators=(',', ':'))
+            json_str = nw.prep_data1(ztype, zcomp)
 
-            self.myprint('sending: ', json_str)
+            nw.myprint('sending: ', json_str)
             resp = await connection.write_message(json_str)
             if (resp is not None) and (len(resp) > 0):
-                self.myprint("Got response: ", resp)
-            self.myprint("Waiting for data...")
+                nw.myprint("Got response: ", resp)
+            nw.myprint("Waiting for data...")
             answer = await connection.read_message()
-            print("Received answer from blockchain")
-            j = json.loads(answer)
-
-            print(j)
-            mtyp_lab = "MessageType"
-            mt = j.get(mtyp_lab)
-            mt_good_answr = "NegotiateConnectionResponse"
-            if mt == mt_good_answr:
-                msg_data_lab = "MessageData"
-                msg_d_answer = j.get(msg_data_lab)
-                msg_negotiate_lab = 'NegotiationStatus'
-
-                final_int = msg_d_answer.get(msg_negotiate_lab)
-                print("all is good")
-                return final_int
+            result = nw.check_answer(answer)
+            if result == 1:
+                print("all is good, was 1, ok to continue")
         except WebSocketClosedError as e:
             print(e)
 
-    async def ws_runner(self, mtype, varss):
+    async def ws_runner(self, mtype, vars):
         if mtype == 1:
-            await self.connect_serve1(varss)                       # in same dir as
+            await self.connect_serve1(vars)                       # in same dir as
 
     def main(self):
-        m_type = nulsuserone.mtype
-        comp_t = nulsuserone.comp_type
-        comp_int = nulsuserone.comp_int
-        vars_list = [comp_t, comp_int]
-        run(self.ws_runner(m_type, vars_list), debug=True)   # starts event loop
+        args_list = [nu.comp_type, str(nu.comp_int)]
+        run(self.ws_runner(nu.mtype, args_list), debug=True)   # starts event loop
 
 
 n = NulsWebsocket()
