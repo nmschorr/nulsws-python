c1e7b3d67b338ad7ead473c1b32262fa0bcdd65d
Nancy Schorr <nms@schorrmedia.com>
2019-12-03 01:02:23 -0800

    changing to library

--- cl.py
+++ cl.py
@@ -15,114 +15,111 @@
 # •  MessageType: The message type, these are specified on section 3]
 # •  MessageData: A Json object that holds the payload of the message.
 
-# This file reads the json file as in dataRequest or dataNegotiateConnection
-# adds info to it and sends it to the server.
 # Note: be careful with typing.Dict - it can cause json problems when converted
+
+# the first object that should be sent - only if the negotiation is ok service may process
+# further requests -otherwise a NegotiateConnectionResponse object should be received with
+# Status set to 0 (Failure) and disconnect immediately.
+# "MessageType": "NegotiateConnection",
+# "MessageData": {
+#     "CompressionAlgorithm": "zlib",
+#     "CompressionRate": "3"
+
+## -- Enter your custom settings data via the library file nulsuserone.py
 
 
 from tornado.websocket import websocket_connect, WebSocketClosedError
-from typing import Dict
 from json import load, dumps
 from asyncio import run
-from random import choices
-from string import ascii_uppercase, digits
-from time import time
-from configdata import ConfigData as cd
-
+from time import time, timezone
+import nulsuserone
+import json
 
 class NulsWebsocket(object):
     def __init__(self):
-        method: str = "ws://"
-        host: str = "127.0.0.1"
-        wport = "9006"
-        port = "7771"
-        dport = "18003"
-        self.my_url: str = ''.join([method, host, ":", port])
-        print("the url:  ", self.my_url)
+        self.ws_url = nulsuserone.my_url
+        self.myprint("the url:  ", self.ws_url)
 
-    def get_top(self, msg_type: int) -> dict:
-        ts = int(time())
-        m_id: str = ''.join(choices(ascii_uppercase + digits, k=15))
-        mtype = cd().get_mdict().get(msg_type)
-        return {"MessageID": m_id, "Timestamp": ts, "TimeZone": -8, "MessageType": mtype}
+    @staticmethod
+    def get_top(msg_type: int) -> dict:
+        the_time = time()
+        tz = timezone
 
-    def read_data_file(self, mtyp: int):
-        fname: str = cd().get_fnames().get(mtyp)
-        print("filename is: ", fname)
-        with open(fname, 'r') as f:
-            mdd = load(f).get("MessageData")
-        mainpart = self.get_top(mtyp)
-        mainpart.update({"MessageData": mdd})     # adding bottom part
-        return mainpart
+        t_stamp = str(int(the_time * 1000))       # change float to int
+        m_id = str(int(the_time * 100000))     # change float to int
+        tzone = str(int(tz / 3600))   # change float to int to str
+        mtype = ' '
+        return {"MessageID": m_id, "Timestamp": t_stamp, "TimeZone": tzone, "MessageType": mtype}
 
+    @staticmethod
+    def make_bottom_part1(c_alg: str, cr_intstr: str):
+        mt_name = "MessageType"
+        mtype = "NegotiateConnection"
+        md_name = "MessageData"
+        c_alg_name = "CompressionAlgorithm"
+        cr_name = "CompressionRate"
+        plist = [ { mt_name : mtype, md_name : { c_alg_name : c_alg, cr_name : cr_intstr} } ]
+        jsds = json.dumps(plist , separators=(',', ':'))
+        print(jsds)
+        return jsds
 
+    def prep_data1(self, ztype, zcomp):
+        all_parts = self.get_top(1)
+        bottom_part = NulsWebsocket.make_bottom_part1(ztype, str(zcomp))
+        all_parts.update({'MessageData': bottom_part})     # adding bottom
+        all_together = ''.join(all_parts)
+        self.myprint('all_parts:  ', all_together)
+        return all_together
 
-
-
-
-    # the first object that should be sent - only if the negotiation is ok service may process
-    # further requests -otherwise a NegotiateConnectionResponse object should be received with
-    # Status set to 0 (Failure) and disconnect immediately.
-    # "MessageType": "NegotiateConnection",
-    # "MessageData": {
-    #     "CompressionAlgorithm": "zlib",
-    #     "CompressionRate": "3"
-
-    def one_lib(self, comp_name = "zlib", comp_int = 3):
-        self.send_negotiate_conn()
-
-
-    def send_negotiate_conn(self, compression_algorithm = "zlib", compression_rate = 3):
-        message_type = "NegotiateConnection"
-        if compression_algorithm not in ["zlib", 0]:
-            compression_algorithm = 0
-            #exit(1)  ## message - wrong compression_algorithm
-        if compression_rate not in [2, 3]: #change later
-            compression_rate = 3
-            exit(1)  ## message - wrong compression_rate
-
-
-        if compression_algorithm == "zlib":
-
-            compression_algorithm = "zlib"
-            compression_rate = 3
-            #
-
-        def myprint(self, x):
+    def myprint(self, x, y=None):
+        debug = True
+        if y is not None:
+            x = x + ' ' + y
+        if debug == True:
             print(x)
 
-        async def connect_serve(self, the_type="zlib", comp = 3) -> None:
-            answer = None
-            try:
-                if the_type != "zlib":  # neg_conn
-                    the_type = "zlib"
-                if comp != 3:  # neg_conn
-                    comp = 3
+    # def one_lib(self, comp_name = 'zlib', comp_int = 3):
+    #     self.send_negotiate_conn()
 
-                connection = await websocket_connect(self.my_url)
-                msg_d = self.read_data_file(the_type)
-                print("sending: ", dumps(msg_d))
-                resp = await connection.write_message(dumps(msg_d))
-                if resp:
-                    print("Got response: ", resp)
-                print("Waiting for data...")
-                answer = await connection.read_message()
-                if answer is not None:
-                    print("Received answer!  :  ", answer)
-            except WebSocketClosedError as e:
-                print(e)
+    async def connect_serve1(self, t_vars):
+        ztype = t_vars[0]
+        zcomp = t_vars[1]
 
-        # waiting for a resp of: type: NegotiateConnectionResponse; NegotiationStatus=1; 1=good
-    async def ws_runner(self, mtype, vars):
-        await self.connect_serve(mtype, vars)                       # in same dir as
+        try:
+            if ztype != 'zlib':  # neg_conn
+                ztype = 'zlib'
+            # if zcomp != 3:  # neg_conn
+            #     zcomp = 3
+            the_url = nulsuserone.my_url
+            connection = await websocket_connect(the_url)
+            ### only continue if connection is ok
+            msg_d = self.prep_data1(ztype, zcomp)
+            self.myprint('sending: ', dumps(msg_d))
+            resp = await connection.write_message(dumps(msg_d))
+            if (resp is not None) and (len(resp) > 0):
+                self.myprint("Got response: ", resp)
+            self.myprint("Waiting for data...")
+            answer = await connection.read_message()
+            # waiting for a resp of: type: NegotiateConnectionResponse; NegotiationStatus=1; 1=good
 
-    def main(self, type, the_vars):
-        run(self.ws_runner(type, the_vars), debug=True)   # starts event loop
+            if answer is not None:
+                print("Received answer!  :  ", answer)
+        except WebSocketClosedError as e:
+            print(e)
+
+    async def ws_runner(self, mtype, varss):
+        if mtype == 1:
+            await self.connect_serve1(varss)                       # in same dir as
+
+    def main(self):
+        m_type = nulsuserone.mtype
+        comp_t = nulsuserone.comp_type
+        comp_int = nulsuserone.comp_int
+        vars_list = [comp_t, comp_int]
+        run(self.ws_runner(m_type, vars_list), debug=True)   # starts event loop
 
 
 n = NulsWebsocket()
-mtype = 1
-comp_type= "zlib"
-comp_int = 3
-the_vars = [comp_type, comp_int]
-n.main(mtype, the_vars)
+n.main()
+## -- enter user data via the library file nulsuserone.py
+
