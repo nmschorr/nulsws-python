64cb68ab7b3afc49b21789eca31d55a55e6be683
Nancy Schorr <nms@schorrmedia.com>
2019-12-07 21:41:21 -0800

    Working on message type register.

--- nulsws_client.py
+++ nulsws_client.py
@@ -25,50 +25,62 @@
 
 
 from asyncio import run
-from tornado.websocket import websocket_connect, WebSocketClosedError
-from nulsws_library import prep_data_type1, prep_data_type3, myprint, check_answer
+from tornado.websocket import websocket_connect  #, WebSocketClosedError
+from nulsws_library import prep_data_type1, prep_data_type3, myprint, check_json_answer
 from nulsws_msgtype1 import websock_url1
-from nulsws_msgtype_register import nulsws_register
-import time
-# import nulsws_msgtype1 as m1
-from nulsws_staticvals import reg
-
-import tornado
+from nulsws_msgtype_register import make_nulsws_reg
 from tornado.websocket import WebSocketClientConnection
-
+import asyncio
 
 class NulsWebsocket(object):
     def __init__(self):
         myprint("the url:  ", websock_url1)
+        self.mindex = 0
 
-
-    async def get_connection(self, json_cnnect, json_b_msg):
-        print("inside get_connection")
+    async def first_connect(self, json_cnnect, the_tup):
+        print("inside first_connect")
         conntn = await websocket_connect(websock_url1)   # 1) CONNECT
+        await asyncio.sleep(2)
         response_awaiteda = await conntn.write_message(json_cnnect) # 2) WRITE
         print("got response_awaiteda for write response...", response_awaiteda)
+        await asyncio.sleep(1.5)
 
-        read_msg = conntn.read_message()    # 3 READ
-       # read_result = check_answer(read_msg)    # 4 CHECK EVALUATE ANSWER
-       # myprint("Got response in get_connection: ", read_result)
-        await self.ws_runner_main(conntn, json_b_msg)
+        await conntn.read_message()    # 3 READ
+        await asyncio.sleep(1)
+        await self.connect_actual_msg(conntn, the_tup)
 
-    async def ws_runner_main(self, websock_connct: WebSocketClientConnection, jsonb: str):
-        print("inside ws_runner_main")
-        resp_await_main = await websock_connct.write_message(jsonb) # 2) WRITE
-        print("got ws_runner_main for write response...", resp_await_main)
-        read_msg = websock_connct.read_message()    # 3 READ
-        read_result = check_answer(read_msg)    # 4 CHECK EVALUATE ANSWER
-        myprint("Got response in ws_runner_main: ", read_result)
+    def nms_callback(self, x):
+        print("inside nms callback: ", x)
+
+
+    async def connect_actual_msg(self, websock_connct: WebSocketClientConnection, a_tup):
+        print("inside connect_actual_msg")
+        print("sending: ", a_tup[0])
+        print(await websock_connct.write_message(a_tup[0])) # 2) WRITE
+        await asyncio.sleep(1)
+        # websock_connct.final_callback(self.nms_callback("final callback msg read in two"))
+        print(await websock_connct.read_message(self.nms_callback("msg read in two")) ) # 3
+        await asyncio.sleep(1)
+
+        print("inside part two connect_actual_msg, sending: ")
+        print(a_tup[1])
+        a = a_tup[1]
+        print(await websock_connct.write_message(a_tup[1])) # 2) WRITE
+        await asyncio.sleep(1)
+        read_msg_b = await websock_connct.read_message(self.nms_callback("msg read b"))  # 3 READ
+        await asyncio.sleep(1)
+        myprint("Got response in ws_runner_main part two: \n", read_msg_b)
         myprint("Finished and exiting.")
 
-    def commander(self, json_st_a, json_st_b):
-        run(self.get_connection(json_st_a, json_st_b))   # starts event loop
+    def commander(self, jconnect, mytup):
+        run(self.first_connect(jconnect, mytup))   # starts event loop
 
     def main(self, mtpe):
+        self.mindex += 1
+        mind = self.mindex
+        json_main_type = None
         json_connect_type = prep_data_type1()
-        if mtpe == 99:
-            json_main_type = nulsws_register()
+        json_reg_type = make_nulsws_reg(mind)
 
         if mtpe == 3:
             json_main_type = prep_data_type3()
@@ -78,13 +90,14 @@ class NulsWebsocket(object):
         if mtpe == 7:
             pass
             # json_main_type = prep_data_type7()
-        self.commander(json_connect_type, json_main_type)
+        json_tup = (json_reg_type, json_main_type)
+        self.commander(json_connect_type, json_tup)
 
 
-# if __name__ == '__main__':
-mtype = 3                       # either 3, 5, or 7, or 99 for r
-n = NulsWebsocket()
-n.main(mtype)
+if __name__ == '__main__':
+    mtype = 3                       # either 3, 5, or 7, or 99 for r
+    n = NulsWebsocket()
+    n.main(mtype)
 
 
 ## -- enter user data via the library file nulsws_msgtype<1,2,3>.py
