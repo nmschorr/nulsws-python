4f2fa164a310d0b53097a76a95caa828deb79d9a
nmschorr <12592910+nmschorr@users.noreply.github.com>
2019-12-11 01:24:27 -0800

    Working on message type register.

--- nulsws_USER_CHOICE.py
+++ nulsws_USER_CHOICE.py
@@ -27,17 +27,22 @@ __ALL__ =[onesy_label, MSG_TYPE, ACCT_ADDRESS]
 from nulsws_CONSTANTS_otherlabels import CHAINID_LABEL as CL
 from nulsws_CONSTANTS_API_names import *
 
-onsies = [("ac_getAddressPrefixByChainId",[CL]), ("ac_getAllAddressPrefix",[]),
-          ("ac_getEncryptedAddressList", [CL]), ("batchValidateBegin", [CL]),
-          ( "clearUnconfirmTxs", [CL]), ("cm_chain", [CL]),(GET_CROSS_CHAIN_SIMPLE_INFOS, []),
-          ("connectReady", []), ("getRegisteredChainInfoList", []), (NW_GET_SEEDS, []),
+onsies = [(AC_GET_ADDRESS_PREFIXBY_CHAINID,[CL]),
+          (AC_GET_ALL_ADDRESS_PREFIX,[]),
+          (AC_GET_ENCRYPTED_ADDRESS_LIST, [CL]),
+          (BATCH_VALIDATE_BEGIN, [CL]),
+          (CLEAR_UNCONFIRMED_TX, [CL]),
+          (CM_CHAIN, [CL]),
+          (CM_GET_CROSS_CHAIN_SIMPLE_INFOS, []),
+          ("connectReady", []),
+          ("getRegisteredChainInfoList", []),
           (NW_GET_MAIN_NET_MAGIC_NUMBER, []),
           ("nw_currentTimeMillis", []),
           (CS_GET_CONSENSUS_CONFIG, []),
 
-          ("crossChainRegisterChange", [CL]),
+          (CROSS_CHAIN_REGISTER_CHANGE, [CL]),
           (CS_GET_AGENT_CHANGE_INFO, [CL]),
-          ("cs_getAgentChangeInfo", [CL]), (CS_GET_CONSENSUS_CONFIG, [CL]), CS_GET_CONSENSUS_CONFIG
+          ("c", [CL]), (CS_GET_CONSENSUS_CONFIG, [CL]),
           (CS_GET_NODE_PACKING_ADDR, [CL]),
           ("nw_getSeeds", []), ("nw_getSeeds", []),("nw_getSeeds", []),
           ("nw_getSeeds", []), ("nw_getSeeds", []), ("nw_getSeeds", []),
