c9cc360c3fcbacc0062d206818c3ebe04c41310a
Nancy Schorr <nms@schorrmedia.com>
2019-12-15 22:20:26 -0800

    New process working

--- Libraries/nulsws_REQUEST.py
+++ Libraries/nulsws_REQUEST.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3.7
 
 from Libraries.nulsws_library import get_TOP_SECTION
-
 from Libraries.Constants.nulsws_CONSTANTS_otherlabels import *
-from UserSettings.nulsws_USER_PARAMS import proto_ver
+from UserSettings.nulsws_USER_PARAMS import USER_CALLS_DB
 
-# from UserSettings.nulsws_USER_PARAMS import _USER_PARAMS
-# from Libraries.Constants.nulsws_CONSTANTS_PARAM_LABELS import *
-from Libraries.Constants.nulsws_CONSTANTS_PARAM_LOOKUP import PARAM_LOOKUP
-# from Libraries.Constants.nulsws_CONSTANTS_API_LABELS import *
+from UserSettings.nulsws_SET import *
+# import UserSettings.nulsws_USER_PARAMS
+# _up = dir(UserSettings.nulsws_USER_PARAMS)
 
 
 # -----------prep_NEGOTIATE_data_type1--------------------------------------#
 def prep_NEGOTIATE_request(msg_indx):   #return dict
-    compress_rate_VALUE = 1
-    compress_type_VALUE = "zlib"
-
 
     # this section has any number of items depending on the msg type
     top_sect = get_TOP_SECTION(1, msg_indx)
@@ -27,7 +22,7 @@ def prep_NEGOTIATE_request(msg_indx):   #return dict
     return top_sect   # dict
 
 # -----------get_REQ_MIDDLE--------------------------------------#
-    # set mid_section_vals in user library file
+
 def get_REQ_MIDDLE(bottom_part, mid_section_vals=None):   #return dict
     if not mid_section_vals:
         mid_section_vals = [ONE, ZERO, ZERO, ZERO, ZERO] #2 = ack+date
@@ -46,62 +41,45 @@ def get_REQ_MIDDLE(bottom_part, mid_section_vals=None):   #return dict
 
 # -----------prep_REQUEST_ONESIE (request) --------------------------------------#
 def prep_REQUEST(msg_indx, api_name_tup):  # requesttype 2 - return ack +
-    # response
-    # onesie either has a second element of a list, or not
-    # user_params_list = uc.user_parameters
+    # response either has a second element of a list, or not
     MSG_TYPE = 3
-    openSQRB = "["
-    closeSQRB = "]"
-    OPENBRKT = "{ "
-    CLOSEBRKT = "}"
-    COMMA = ", "
-    COLON = ": "
-    COLON = ": "
-    beginn = " "
-    endd = " :{} }"
-
     api_name = api_name_tup[0]
-    api_name_word = api_name_tup[1]
+    api_text = api_name_tup[1]
     myparams = []
-    msg_section_bottom = ''
 
-    for i in PARAM_LOOKUP:
-        if i[0] == api_name_word:
-            myparams = i[1]
-            for tup in myparams:
-                print(tup[0], tup[1])
+    kv = USER_CALLS_DB
+
+    for uc in USER_CALLS_DB:
+        if uc[0] == api_text:
+            myparams = uc[1]   # part two is the params list
             break  # found it
 
     params_len = len(myparams)
-    print("params len: ", params_len)
+    temp_dict = {}
+    e = dict()
+    msg_section_bottom = ''
     # -------------------------------------Length One ---------------------------------
     if params_len == 0:
-        print("doing a 0 params", api_name, ": ", myparams)
-        api_name_word = str(myparams)
-        midd: str = api_name_word + ":{}"
-        msg_section_bottom.join(f'{OPENBRKT}{midd}{CLOSEBRKT}{closeSQRB}')
+        msg_section_bottom = dict({api_text: e})
 
-    else:
-        # msg_section_bottom: str = " [{ " + api_name_word + ": "
-        msg_section_bottom = openSQRB
+    elif params_len > 0:
+        for par in range(params_len):
+            print("doing multiple params", api_name, ": ", myparams, "paramslen: ", params_len)
 
-        for x in range(params_len):
-            print("doing a 0 params", api_name, ": ", myparams)
-            p1 = str(myparams[0][0])
-            p2 = str(myparams[0][1])
-            midd: str = f"{api_name_word}" + ": "
-            msg_section_bottom = f'{openSQRB}{OPENBRKT}{midd}{CLOSEBRKT},'
+            p1 = str(myparams[par][0])
+            p2 = str(myparams[par][1])
+            temp_dict = dict({p1: p2})
 
-            ddline = f'{OPENBRKT}{p1}{COLON}{p2}{closeSQRB}'
-            msg_section_bottom += ddline
-            # print()
-        msg_section_bottom = msg_section_bottom + ']'
+        msg_section_bottom = dict({api_text: e})
+        msg_section_bottom.__setitem__(api_text, temp_dict)
 
     msg_section_MIDDLE = get_REQ_MIDDLE(msg_section_bottom)
+    a=0
     message_section_TOP = get_TOP_SECTION(MSG_TYPE, msg_indx)
+
     message_section_TOP.update(msg_section_MIDDLE)
     print(message_section_TOP)
-    return message_section_TOP  # return dict
+    return message_section_TOP  # "'"return dict
 
 # ----------- end library file --------------------------------------#
 
